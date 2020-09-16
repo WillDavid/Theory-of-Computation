6d18e3dcde4c1266c4610355a1fd81c1429bf87b
WillDavid <wdma.lic17@uea.edu.br>
2020-09-16 14:06:36 -0400

    Update At2.2.py

--- William_David/Projeto02/At2.2.py
+++ William_David/Projeto02/At2.2.py
@@ -1,6 +1,10 @@
-string = "}["
+string = "[{}])(()"
 pilha = []
+lista = []
 casado = True
+correto = True
+
+
 for i in string:
     
     if i== "(" or i=="[" or i=="{":
@@ -18,38 +22,55 @@ for i in string:
         else:
             casado = False
 
+if (len(pilha) != 0):
+    casado = False
 #parte 2
-lista = []
+
 for i in string:
     lista.append(i)
-
-d = [('(', "3"), (')', "4"),('[', "2"), (']', "4"), ('{', "1"), ('}', "6")]
+d = [('(', "3"), (')', "4"),('[', "2"), (']', "5"), ('{', "1"), ('}', "6")]
 dicio = dict(d)
 
 s = ''
 for i in lista:
     s += dicio[i]
 
-correto = True
 for i in range(len(s)):
-    if s[i] == "3":
-        if s[i+1] == "1" or s[i+1] == "2":
-            correto = False
-            break
-    elif s[i] == "2":
-        if s[i+1] == "1" or s[i+1] == "6":
-            correto = False
-            break
-    elif s[i] == "1":
-        print("to vendo")
-    else:
-        print("nao faz nada")
+    if(casado):
+        if s[i] == "3":
+            if s[i+1] == "1" or s[i+1] == "2":
+                correto = False
+                break
+        elif s[i] == "2":
+            if s[i+1] == "1" or s[i+1] == "6":
+                correto = False
+                break
+        elif s[i] == "1":
+            print("to vendo")
+        else:
+            print("nao faz nada")
+
+
+#Verificacao
 
 if (len(pilha) == 0) and (casado == True):
     if(correto):
         print("casada e correta")
     else:
         print("casada e incorreta")
+        valoresNumero = []
+
+        #Trabalhando nessa parte
+        for i in range(len(s)):
+            valoresNumero.append(s[i])
+
+        print(valoresNumero)
+        result = sorted(valoresNumero)
+        print(result)        
 else:
     print("nao casada")
+
+
+
+
 
