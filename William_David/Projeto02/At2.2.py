809a6c47645bc1a9f446f66d8f001ae45189c69f
WillDavid <wdma.lic17@uea.edu.br>
2020-09-16 00:19:12 -0400

    Update At2.2.py

--- William_David/Projeto02/At2.2.py
+++ William_David/Projeto02/At2.2.py
@@ -1,4 +1,4 @@
-string = "([{}])"
+string = "{[()[]"
 pilha = []
 correto = True
 for i in string:
@@ -8,20 +8,48 @@ for i in string:
         topo = i
     else:
         if pilha != []:
-            if (i == ")" and topo == "(" ) or (i=="]" and i=="[") or (i=="}" and i=="{"):    
+            if (i == ")" and topo == "(" ) or (i=="]" and topo=="[") or (i=="}" and topo=="{"):    
                 if len(pilha) > 0:
                     pilha.pop()
+                    if len(pilha) > 0:
+                        topo = pilha[len(pilha)-1]
             else:
                 correto = False
         else:
             correto = False
-    print(pilha)             
 
 if (len(pilha) == 0) and (correto == True):
     print("casada")
 else:
     print("nao casada")
 
+lista = []
+for i in string:
+    lista.append(i)
+print(lista)
 
+d = [('(', "1"), (')', "1"),('[', "2"), (']', "2"), ('{', "3"), ('}', "3")]
+dicio = dict(d)
 
-    #Se eu tenho ( )
+s = ''
+for i in lista:
+    s += dicio[i]
+
+print(s)
+
+# Will Tentativa
+for i in range(len(s)):
+    if(s[i] == "1"):
+        if(s[i] != [0]):
+            if(s[i-1] == "2" or s[i-1] == "3" or s[i-1] == "1"):
+                print("tem") #lado esquerdo
+    
+
+    elif(s[i] == "2"):
+        if(s[i] != [0]):
+            if(s[i-1] == "3" or  s[i-1] == "2"):
+                print("tem") #lado esquerdo
+    
+
+            
+            
